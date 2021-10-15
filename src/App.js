422619d71065f674fb9fe8566a0f42311f54613e
hrishikeshsoman <hrishikeshsoman3@gmail.com>
2021-10-15 20:09:31 +0530

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <pre>
+          hrishikeshsoman.work in progress...
+          version 0.0.1
+        </pre>
+      </header>
+    </div>
+  );
+}
+
+export default App;
